3eecaa5173519b144f77a995a5f67e4db639b1c6
Milosz Kulesza <milosz.kulesza1@gmail.com>
2020-05-22 12:55:58 +0200

    Obsługa pliku z ustawieniami aplikacji

--- RocketChatUWP.Core/Api/RocketChatRestApi.cs
+++ RocketChatUWP.Core/Api/RocketChatRestApi.cs
@@ -4,57 +4,80 @@ using RocketChatUWP.Core.ApiModels;
 using RocketChatUWP.Core.Services;
 using System;
 using System.IO;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Data.Xml.Dom;
+using Windows.Storage;
 using Windows.UI.Notifications;
 
 namespace RocketChatUWP.Core.Api
 {
     public class RocketChatRestApi : IRocketChatRestApi
     {
-        private string serverUrl = "http://192.168.0.177:3000";
+        private string serverAddress;
         private readonly IToastNotificationsService toastService;
 
         public RocketChatRestApi(IToastNotificationsService toastService)
         {
             this.toastService = toastService;
-            //var directory = Directory.GetCurrentDirectory();
-            //var path = Path.Combine(directory, "settings.json");
-            //if (File.Exists(path))
-            //{
-            //    dynamic content = JsonConvert.DeserializeObject(File.ReadAllText(path));
-            //    if (content != null)
-            //        serverUrl = content.settings.serverAddress;
-            //    else
-            //    {
-            //        var newContent = new
-            //        {
-            //            settings = new
-            //            {
-            //                serverAddress = "http://192.168.0.177:3000"
-            //            }
-            //        };
-            //        var json = JsonConvert.SerializeObject(newContent);
-            //        File.WriteAllText(path, json, Encoding.UTF8);
-            //        serverUrl = newContent.settings.serverAddress;
-            //    }
-            //}
-            //else
-            //{
-            //    var content = new
-            //    {
-            //        settings = new
-            //        {
-            //            serverAddress = "http://192.168.0.177:3000"
-            //        }
-            //    };
-            //    var json = JsonConvert.SerializeObject(content);
-            //    File.WriteAllText(path, json, Encoding.UTF8);
-            //    serverUrl = content.settings.serverAddress;
-            //}
+            SetServerAddress();  
+        }
+
+        private async void SetServerAddress()
+        {
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFile file = await storageFolder.CreateFileAsync("settings.json", CreationCollisionOption.OpenIfExists);
+            if (file.IsAvailable)
+            {
+                dynamic content = JsonConvert.DeserializeObject(await FileIO.ReadTextAsync(file));
+                if (content != null)
+                {
+                    if(content.settings.restServerAddress != null)
+                        serverAddress = content.settings.restServerAddress;
+                    else
+                    {
+                        var newContent = new
+                        {
+                            settings = new
+                            {
+                                restServerAddress = "http://192.168.0.177:3000"
+                            }
+                        };
+                        var json = JsonConvert.SerializeObject(newContent);
+                        await FileIO.WriteTextAsync(file, json);
+                        serverAddress = newContent.settings.restServerAddress;
+                    }
+                }
+                else
+                {
+                    var newContent = new
+                    {
+                        settings = new
+                        {
+                            restServerAddress = "http://192.168.0.177:3000"
+                        }
+                    };
+                    var json = JsonConvert.SerializeObject(newContent);
+                    await FileIO.WriteTextAsync(file, json);
+                    serverAddress = newContent.settings.restServerAddress;
+                }
+            }
+            else
+            {
+                var content = new
+                {
+                    settings = new
+                    {
+                        restServerAddress = "http://192.168.0.177:3000"
+                    }
+                };
+                var json = JsonConvert.SerializeObject(content);
+                await FileIO.WriteTextAsync(file, json);
+                serverAddress = content.settings.restServerAddress;
+            }
         }
 
         public async Task<bool> Login(string username, string password)
@@ -71,7 +94,7 @@ namespace RocketChatUWP.Core.Api
                 HttpResponseMessage response = new HttpResponseMessage();
                 try
                 {
-                    response = await client.PostAsync($"{serverUrl}/api/v1/login", content);
+                    response = await client.PostAsync($"{serverAddress}/api/v1/login", content);
                 }
                 catch
                 {
